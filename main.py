87629685442252749262affcf68bd27e8bac6bee
Jean-David <jeandavidmoisan@gmail.com>
2016-02-04 20:29:28 -0500

    Updated Gui, along with the missing CompetitionH2016 code.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-#import sys
-#sys.path.append(".")
-from RULEngine.Framework import start_game
-from UltimateStrategy import UltimateStrategy
+#!/usr/bin/python
+import game_launcher
+from RULEngine.Util.Pose import Pose
+from RULEngine.Util.Position import Position
+from RULEngine.Game.Player import Player
 
-__author__ = 'jbecirovski'
+class Defi1(object):
 
-if __name__ == '__main__':
-    start_game(UltimateStrategy, gui=True)
+    a = 0
+    def boucle_principale(self, coach, terrain, etats, equipe_bleu, equipe_jaune):
+        if self.a == 0:
+            self.a = 1
+            #coach.bouger(0, terrain.ball)  #bouger vers la balle en conservant l'orientation de depart
+            #coach.bouger(0, terrain.ball, cible=terrain.ball)  #bouger vers la balle en visant la balle
+            #coach.bouger(0, Position(), cible=terrain.ball)    #bouger vers le centre en visant la balle
+            #coach.chercher_balle(1)
+            #coach.lancer(2, Position(-3000,0))
+            coach.passe(0,3)
+            if(coach.robot_events[0] == "success" and coach.robot_events[3] == "success" ):
+                coach.passe(3,0)
 
+defi = Defi1()
+game_launcher.start_game(defi.boucle_principale)
